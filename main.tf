7a71c948f8aaee53180a32bf907ef98d90850fcf
esthermurambikar <anupriyamurambikar@gmail.com>
2024-07-11 22:38:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 7000
+    to_port     = 7000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
